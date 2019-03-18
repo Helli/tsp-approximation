7f79e732528b64ff6f5f3430c6d2bb844489e740
helli <hellauer@in.tum.de>
2019-03-18 22:33:26 +0100

    copied from Kruskal, adapted

--- Specification.thy
+++ Specification.thy
@@ -3,9 +3,18 @@ theory Specification
   imports
     Koenigsberg_Friendship.KoenigsbergBridge
     Kruskal.Graph_Definition_Aux
-begin
+begin hide_const a b c d
 
-hide_const a b c d
+subsection \<open>Adapting Definitions\<close>
+
+fun is_path_undir' :: "('v, 'w) graph \<Rightarrow> 'v \<Rightarrow> ('v,'w) path \<Rightarrow> 'v \<Rightarrow> bool" where
+    "is_path_undir' G v [] v' \<longleftrightarrow> v=v' \<and> v'\<in>nodes G" |
+    "is_path_undir' G v ((v1,w,v2)#p) v'
+       \<longleftrightarrow> v=v1 \<and> ((v1,w,v2)\<in>edges G
+         \<and> (v2,w,v1)\<in>edges G \<comment> \<open>not yet sure if this is good\<close>
+       ) \<and> is_path_undir' G v2 p v'"
+
+abbreviation "nodes_connected' G a b \<equiv> \<exists>p. is_path_undir' G a p b"
 
 term nodes
 
