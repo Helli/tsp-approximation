f4bd72cc152b22df860ad3e367ea91674196392d
helli <f.hellauer@tum.de>
2019-03-18 18:28:27 +0100

    move document to https://sharelatex.tum.de/project/5c8f93d0163cf2bd10432579 for faster compilation and easier previews. (If I miss antiquotations too much, I can always switch back.)

--- thesis/Thesis.thy
+++ thesis/Thesis.thy
@@ -1,28 +1,6 @@
 (*<*)theory Thesis imports "TSP.Specification" begin(*>*)
 
+\<comment> \<open>moved to @{url "https://sharelatex.tum.de/project/5c8f93d0163cf2bd10432579"}\<close>
 text\<open>dummy citation:~@{cite fixme}\<close>
-
-section \<open>MST Heuristic\<close>
-
-text \<open>Since the removal of an edge transforms a TSP tour into a spanning tree, the Minimum
- Spanning Tree (MST) of a complete graph always has a summed length less than or equal to the
- optimal TSP tour.
-
-This observation gives rise to an easy 2-approximation(to-do:explain): Generate a minimum spanning
- tree, then visit the cities along its edges. A DFS(to-do: explain *once* in the thesis) on the
- tree does this. It uses every edge twice (once on discovering an edge, and once on backtracking from
- it). Thus it generates a sequence with at most twice the cost of the optimal one.
-
-Duplicate visits to cities can be dropped afterwards. This does not increase
-the cost, as these shortcuts satisfy the triangle inequality.
-\<close>
-
-section \<open>Metrics, Input graph\<close>
-
-text \<open>To generate a graph that satisfies the triangle inequality, we put some points into a
- space with integer coordinates and compute the pairwise distances. Special care is to be put in the
-choice of the metric function: The standard euclidean distance may generated graph edges with
- irrational weights, which are difficult to represent in way that allows easy comparison.
-\<close>
 
 (*<*)end(*>*)
