f5dfb452b5fb20b282c29b1586fd37492685fa2a
helli <hellauer@in.tum.de>
2019-03-19 01:04:25 +0100

    use locale targets. resolve some comments

--- Specification.thy
+++ Specification.thy
@@ -20,21 +20,34 @@ abbreviation "nodes_connected' G a b \<equiv> \<exists>p. is_path_undir' G a p b
 
 definition symhull where
   "symhull G = G\<lparr>edges := {(v1,w,v2) | v1 w v2. (v1,w,v2) \<in> edges G \<or> (v2,w,v1) \<in> edges G}\<rparr>"
-    \<comment> \<open>to-do: use \<^const>\<open>hull\<close>\<close>
 
-lemma
-  assumes "valid_mgraph G"
-  assumes no_id[simp]:"\<And>v w.(v,w,v) \<notin> edges G"
+lemma (in valid_graph) valid_unMultigraph_symhull:
+  assumes no_id[simp]:"\<And>v w.(v,w,v) \<notin> E"
   shows "valid_unMultigraph (symhull G)"
   apply unfold_locales
      apply (auto simp: symhull_def)
-     apply (simp_all add: assms(1) valid_graph.E_validD(1) valid_mgraph.axioms)
-   apply (simp_all add: assms(1) valid_graph.E_validD(2) valid_mgraph.axioms)
-  done
+  using E_validD by blast+
 
-(*
-definition tour
-*)
+lemma (in valid_graph) symhull_altdef:
+  assumes no_id:"\<And>v w.(v,w,v) \<notin> E"
+  shows "symhull G = G\<lparr>edges := (\<lambda>E. valid_unMultigraph (G\<lparr>edges := E\<rparr>)) hull E\<rparr>"
+proof -
+  have "edges (symhull G) = (\<lambda>E. valid_unMultigraph (G\<lparr>edges := E\<rparr>)) hull E"
+    apply (simp add: symhull_def)
+    apply (rule hull_unique[symmetric])
+    apply auto
+     apply (metis no_id symhull_def valid_unMultigraph_symhull)
+  proof goal_cases
+    case (1 t' v1 w v2)
+    then have "(v2, w, v1) \<in> t'"
+      by blast
+    with "1"(2) have "(v1, w, v2) \<in> t'"
+      using valid_unMultigraph.corres by fastforce
+    then show ?case.
+  qed
+  then show ?thesis
+    by (simp add: symhull_def)
+qed
 
 text \<open>Citation test: @{cite lawler}.\<close>
 
