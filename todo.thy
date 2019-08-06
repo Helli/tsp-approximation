6922d0a45d33a6ad07a469ce245b8b3bdc6e582f
helli <f.hellauer@tum.de>
2019-08-06 17:50:48 +0200

    notes from the meeting

--- todo.thy
+++ todo.thy
@@ -2,8 +2,15 @@ theory todo imports DFS_Phase
 begin
 
 \<comment> \<open>Change the extended state to store the (pre)tour @{type list} instead of a @{type bool}...\<close>
+
+section \<open>Thesis\<close>
 \<comment> \<open>SMS notes\<close>
 
-Is Prim_Dijkstra_Simple useful?
+\<comment> \<open>go from the abstract to concrete, also within the sections/components\<close>
+\<comment> \<open>getting more detailed along the way.\<close>
+
+\<comment> \<open>For Prim_Dijkstra_Simple, a small reference is enough or maybe one paragraph; in theory it looks usable just like Kruskal.\<close>
+
+\<comment> \<open>use clear referencing: Five sentences / five lines (approximately) in the abstract become five paragraphs in the overview, and five chapters (which all start with abstracts, too).\<close>
 
 end
